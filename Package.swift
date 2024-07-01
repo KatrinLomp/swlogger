c525c256344a07d3bf7bc161de8ebc21a7b22ff7
Katrin Annuk <katrin.annuk@lab.mobi>
2024-07-01 21:50:33 +0300

    Update package spec

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SWLogger",
     platforms: [
-       .iOS(.v15),
+       .iOS(.v15), .macOS(.v11)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
